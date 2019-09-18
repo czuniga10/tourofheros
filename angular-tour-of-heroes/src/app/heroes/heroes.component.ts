3628886556c62d97f5e3b4b908bd0b0f0fd10551
Chad Zuniga <chad.zuniga@progleasing.com>
2019-09-18 10:06:23 -0600

    removed detail concerns from Hero Component created hero-detail component to take over details

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -8,17 +8,17 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+
   selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-  };
-
-  heroes= HEROES;
+  }
 
   constructor() { }
 
   ngOnInit() {
   }
-  
+
 
 }
