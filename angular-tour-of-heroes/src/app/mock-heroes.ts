5d1f01893fcffcb57bb6a9ff16ccf945a90f2e6b
Chad Zuniga <chad.zuniga@progleasing.com>
2019-09-23 13:09:58 -0600

    init in memory data

--- angular-tour-of-heroes/src/app/mock-heroes.ts
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -1,14 +1,19 @@
-import { Hero } from './hero';
+// using a mock api to retrieve data now (in-memory-data-service)
 
-export const HEROES: Hero[] = [
-    { id: 11, name: 'Dr Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
+
+
+// import { Hero } from './hero';
+
+// export const HEROES: Hero[] = [
+//     { id: 11, name: 'Dr Nice' },
+//     { id: 12, name: 'Narco' },
+//     { id: 13, name: 'Bombasto' },
+//     { id: 14, name: 'Celeritas' },
+//     { id: 15, name: 'Magneta' },
+//     { id: 16, name: 'RubberMan' },
+//     { id: 17, name: 'Dynama' },
+//     { id: 18, name: 'Dr IQ' },
+//     { id: 19, name: 'Magma' },
+//     { id: 20, name: 'Tornado' }
+// ]
+// ;
